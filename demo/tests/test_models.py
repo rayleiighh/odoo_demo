a4a2ad92ea284a48824a4bf98c126879bd8b4b8a
rayleiighh <r.benlhaj@students.ephec.be>
2025-11-12 15:37:43 +0100

    [FIX] correction du assertEqual

--- demo/tests/test_models.py
+++ demo/tests/test_models.py
@@ -29,7 +29,7 @@ class TestUsers(TransactionCase):
                     'name': 'Marie-Noël',
                     'login': 'mnv',
                 })
-                self.assertEqual(user.description, None)
+                self.assertTrue(user.description)
 
 
     def test_description_one_line(self):
